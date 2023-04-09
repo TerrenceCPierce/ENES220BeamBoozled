838043ebedca973175bb1e82a1eb6ddb4da72448
TerrenceCPierce <65622821+TerrenceCPierce@users.noreply.github.com>
2023-04-09 14:12:01 -0400

    Safety factors and Strength to Weight
    Strength to Weight is still a work in progress because it uses the user input Force, eventually it will calculate the force in which the beam has a safety factor of 1 in.

--- BeamCalc.m
+++ BeamCalc.m
@@ -68,12 +68,12 @@ maxM_right = subs(MRight);
 
 maxMArr = [maxM_left;maxM_right];
 [~,X] = max(abs(maxMArr));
-M_max =  maxMArr(X);
+M_max =  vpa(maxMArr(X));
 
 if abs(VLeft)>abs(VRight)
-    V_max = VLeft;
+    V_max = vpa(VLeft);
 else
-    V_max = VRight;
+    V_max = vpa(VRight);
 end
 
 maxDeflect_x = solve(slopeLeft);
@@ -82,4 +82,32 @@ maxDeflect = vpa(subs(deflectLeft));
 
 %Now do max stress calculations for tensile, shear, and glue shear
 
+%This assumes strength is the same in compression and tension
+%this assumption may not hold for pine
+bendStress_flange = -M_max/I*(flange_height+web_height/2);
+bendStress_web = -M_max/I*(web_height/2);
 
+%shear and glue
+Q_joint = flange_width*flange_n*flange_height*(flange_height/2+web_height/2);
+Q_max = Q_joint + web_width*web_n*web_height*web_height/4;
+
+shearStress_max = V_max*Q_max/(web_n*I*web_width); %middle of I
+shearStress_flange = V_max*Q_joint/(flange_n*I*flange_width); %connection between flange and web
+shearStress_glue = V_max*Q_joint/(web_n*I*web_width); %connection between flange and web
+%not sure if I should multiple last equation by web_n or not
+
+%calculate safety factors
+SF_bendStress = Oak_Tens_Avg/abs(bendStress_flange);
+SF_shearStress = min(Oak_Shear_Avg/abs(shearStress_max) + Oak_Shear_Avg/abs(shearStress_flange));
+SF_shearStressGlue = OakGlue_Shear_Avg/abs(shearStress_glue);
+
+flangeVolume = L_tot*flange_width*flange_height;
+FlangeWeight = flangeVolume*dens_Oak;
+
+webVolume = L_tot*web_width*web_height;
+webWeight = webVolume*dens_Oak;
+
+weight = 2*FlangeWeight + webWeight;
+
+%This is not accurate because this F is not the F that the beam breaks
+str2Weight = F/weight;
